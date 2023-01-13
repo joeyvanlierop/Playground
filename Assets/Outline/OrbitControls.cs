e52ea4b8afadf041fd549dccceff696e9315c571
Joey Van Lierop <joey.vanlierop@me.com>
2023-01-12 23:03:46 -0700

    Small improvements

--- Assets/Outline/OrbitControls.cs
+++ Assets/Outline/OrbitControls.cs
@@ -2,33 +2,20 @@ using UnityEngine;
 
 namespace Outline
 {
-    /// <summary>
-    /// A simple free camera to be added to a Unity game object.
-    /// 
-    /// Keys:
-    ///	wasd / arrows	- movement
-    ///	q/e 			- up/down (local space)
-    ///	r/f 			- up/down (world space)
-    ///	pageup/pagedown	- up/down (world space)
-    ///	hold shift		- enable fast movement mode
-    ///	right mouse  	- enable free look
-    ///	mouse			- free look / rotation
-    ///     
-    /// </summary>
     public class OrbitControls : MonoBehaviour
     {
-        [SerializeField, Range(1, 10)]
-        private float rotateSensitivity = 10f;
-        [SerializeField, Range(1, 5)]
-        private float panSensitivity = 1f;
-        [SerializeField, Range(1, 5)]
-        private float zoomSensitivity = 1f;
-        [SerializeField]
-        private bool invertZoom = true;
-        private bool panning = false;
-        private bool orbiting = false;
+        [SerializeField] [Range(1, 10)] private float rotateSensitivity = 10f;
 
-        void Update()
+        [SerializeField] [Range(0, 1)] private float panSensitivity = 1f;
+
+        [SerializeField] [Range(0, 1)] private float zoomSensitivity = 1f;
+
+        [SerializeField] private bool invertZoom = true;
+
+        private bool orbiting;
+        private bool panning;
+
+        private void Update()
         {
             if (panning)
             {
@@ -47,28 +34,15 @@ namespace Outline
 
             var axis = Input.GetAxis("Mouse ScrollWheel");
             axis *= invertZoom ? -1 : 1;
-            if (axis != 0)
-            {
-                Camera.main.orthographicSize += zoomSensitivity * axis;
-            }
-
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                orbiting = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse0))
-            {
-                orbiting = false;
-            }
+            if (axis != 0) Camera.main.orthographicSize += zoomSensitivity * axis;
 
             if (Input.GetKeyDown(KeyCode.Mouse1))
-            {
+                orbiting = true;
+            else if (Input.GetKeyUp(KeyCode.Mouse1)) orbiting = false;
+
+            if (Input.GetKeyDown(KeyCode.Mouse2))
                 panning = true;
-            }
-            else if (Input.GetKeyUp(KeyCode.Mouse1))
-            {
-                panning = false;
-            }
+            else if (Input.GetKeyUp(KeyCode.Mouse2)) panning = false;
 
             if (panning || orbiting)
             {
