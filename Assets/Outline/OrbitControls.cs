670f928807cea5548b0b4ff8852ab1ce8fb5b02d
Joey Van Lierop <joey.vanlierop@me.com>
2023-01-12 23:15:05 -0700

    Minor refactoring

--- Assets/Outline/OrbitControls.cs
+++ Assets/Outline/OrbitControls.cs
@@ -5,11 +5,9 @@ namespace Outline
     public class OrbitControls : MonoBehaviour
     {
         [SerializeField] [Range(1, 10)] private float rotateSensitivity = 10f;
-
         [SerializeField] [Range(0, 1)] private float panSensitivity = 1f;
-
         [SerializeField] [Range(0, 1)] private float zoomSensitivity = 1f;
-
+        [SerializeField] private bool invertPan = true;
         [SerializeField] private bool invertZoom = true;
 
         private bool orbiting;
@@ -18,23 +16,10 @@ namespace Outline
         private void Update()
         {
             if (panning)
-            {
-                var newPositionX = Input.GetAxis("Mouse X");
-                var newPositionY = Input.GetAxis("Mouse Y");
-                transform.Translate(new Vector3(newPositionX, 0, 0) * panSensitivity);
-                transform.Translate(new Vector3(0, newPositionY, 0) * panSensitivity, Space.World);
-            }
-
+                Pan();
             if (orbiting)
-            {
-                var newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotateSensitivity;
-                var newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * rotateSensitivity;
-                transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
-            }
-
-            var axis = Input.GetAxis("Mouse ScrollWheel");
-            axis *= invertZoom ? -1 : 1;
-            if (axis != 0) Camera.main.orthographicSize += zoomSensitivity * axis;
+                Orbit();
+            Zoom();
 
             if (Input.GetKeyDown(KeyCode.Mouse1))
                 orbiting = true;
@@ -54,6 +39,29 @@ namespace Outline
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
             }
+        }
+
+        private void Pan()
+        {
+            var newPositionX = Input.GetAxis("Mouse X");
+            newPositionX *= invertPan ? -1 : 1;
+            var newPositionY = Input.GetAxis("Mouse Y");
+            transform.Translate(new Vector3(newPositionX, 0, 0) * panSensitivity);
+            transform.Translate(new Vector3(0, newPositionY, 0) * panSensitivity, Space.World);
+        }
+
+        private void Orbit()
+        {
+            var newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotateSensitivity;
+            var newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * rotateSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        private void Zoom()
+        {
+            var zoomAxis = Input.GetAxis("Mouse ScrollWheel");
+            zoomAxis *= invertZoom ? -1 : 1;
+            if (zoomAxis != 0) Camera.main.orthographicSize += zoomSensitivity * zoomAxis;
         }
     }
 }
